9ed3275af7370d3429b4c7662df676cb8cc612d0
TimeLord <836444684@qq.com>
2024-08-18 11:23:46 -0400

    "test tool and sppedinsight"

--- src/app/skill/experience/page.jsx
+++ src/app/skill/experience/page.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Entry from "@/app/components/Entry/Entry";
 import SampleIntroduction from "@/app/components/SampleIntroduction/SampleIntroduction";
 import { AiFillGithub, AiFillLinkedin, AiOutlineX } from "react-icons/ai";
